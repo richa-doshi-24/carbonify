f8db831facb9f57fc30b1df1f2b57afaf2139aed
Richa Doshi <richadoshi@10-18-174-49.dynapool.wireless.nyu.edu>
2023-02-18 21:34:54 -0500

    Adding navigation bar

---
+++ ui/src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is page 1!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
